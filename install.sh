fb32c8cd2e5101dd2886309149e234daea0dd8fc
A3gisS3c <bjc.bettuzzi@gmail.com>
2024-07-17 06:00:52 +0200

    First commit

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ if [ "$(/usr/bin/id -u)" != "0" ]; then
 	exit 1
 fi
 
-if ! (/usr/bin/which node && /usr/bin/which apache2 && /usr/bin/which yarn); then
+if ! (/usr/bin/which node && /usr/bin/which apache2 && /usr/bin/which yarn && dpkg -l | grep libpam0g-dev); then
 	/usr/bin/echo "Apache2 or Nodejs or npm or yarn or libpam0g-dev not installed - abort installation" 1>&2
 	exit 1
 fi
@@ -40,13 +40,16 @@ fi
 if [ -z "$(ls -A /etc/yasd)" ]; then
 	/usr/bin/echo "Copying configuration files in /etc/yasd/" 1>&2
 	/usr/bin/cp /opt/YASD/config/yasd.conf /opt/YASD/config/yasdUI.conf /etc/yasd/
-	/usr/bin/chmod -R 640 /etc/yasd 
+	/usr/bin/chmod -R 640 /etc/yasd
+	SECRET=$(/usr/bin/openssl rand -hex 64)
+	/usr/bin/sed -i "s/JWTSECRET/$SECRET/g" /etc/yasd/yasd.conf
 else
 	/usr/bin/echo "/etc/yasd/ folder not empty. Skipping copying configuration files" 1>&2
 fi
 
 ### YASD frontend
 /usr/bin/mkdir -p /var/www/yasd/
+/usr/bin/echo "Installing dependencies" 1>&2
 /usr/local/bin/yarn --cwd /opt/YASD/yasd-back install
 /usr/local/bin/yarn --cwd /opt/YASD/yasd-front install
 /usr/local/bin/yarn --cwd /opt/YASD/yasd-front build
@@ -65,4 +68,10 @@ if [ ! -f /etc/systemd/system/yasd.service ]; then
 else
 	/usr/bin/echo "Yasd Systemd service already exists. Skipping" 1>&2
 fi
+
+### Final echos
+echo "#########################################################################################" 1>&2
+echo "Installation finished" 1>&2
+echo "Do not forget to edit Apache SSL (cert/key/server_name...) and systemctl reload Apache" 1>&2
+echo "Do not forget to edit /etc/yasd/yasd.conf and systemctl start yasd" 1>&2
 
